16923c5e11745472ba753683400aa6390ac6232c
fel <Felix.Langenegger@ti8m.ch>
2015-08-17 13:40:33 +0200

    Remove debug, fixed regex

--- app/models/converter.rb
+++ app/models/converter.rb
@@ -17,27 +17,25 @@ class Converter < ActiveRecord::Base
     csv_string = CSV.generate do |csv|
       csv << ["Story Name", "Est. Time", "Used Time", "List Name"]
       json["cards"].each do |card|
+
         row = []
         list_name = ""
-        name = card["name"]
-        puts name
-        name.match(/(\[)(\d+\.?\d?)(\/)(\d?\.?\d?-?)(\])(.+)/)
-        puts $6
-        puts $2
-        puts $4
-        row << $6.strip
-        row << $2.strip
-        row << $4.strip
+
         json["lists"].each do |list|
           if list["id"] == card["idList"]
             list_name = list["name"]
-            puts list_name
           end
         end
-        if list_name != "Impendence" || list_name != "Technical Debts"
-          row << list_name
-          csv << row
-        end
+
+        name = card["name"]
+        name.match(/(\[?)(\d*\.?\d*)(\/?)(\d*\.?\d*-?)(\]?)(.+)/)
+        row << $6.strip
+        row << $2.strip
+        row << $4.strip
+        row << list_name
+
+        csv << row
+
       end
     end
     csv_string
